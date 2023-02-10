ea2d19f10826d15eb88b3c12e619ac446be6cf56
Jordi Pradel <jordi.pradel@agilogy.com>
2023-02-10 12:09:34 +0100

    Add missing files

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,17 @@
+object Dependencies {
+
+    val arrowKt = "io.arrow-kt"
+    val arrowVersion = "2.0.0-SNAPSHOT"
+    val arrowCore = "$arrowKt:arrow-core:$arrowVersion"
+
+    val kolinx = "org.jetbrains.kotlinx"
+    val kotlinxSerializationVersion = "1.4.1"
+    val kotlinXSerializationJson = "$kolinx:kotlinx-serialization-json:$kotlinxSerializationVersion"
+    val kotlinxCoroutinesVersion = "1.6.4"
+    val kotlinXCoroutinesCore = "$kolinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion"
+
+    val kotest = "io.kotest"
+    val kotestVersion = "5.5.5"
+    val kotestRunnerJunit = "$kotest:kotest-runner-junit5:$kotestVersion"
+
+}
