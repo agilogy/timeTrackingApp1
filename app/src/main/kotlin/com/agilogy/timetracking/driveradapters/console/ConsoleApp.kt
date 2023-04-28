9b4a55462a2a9f3bdc17c2029f1f3bfbdd352f9e
Jordi Pradel <jordi.pradel@agilogy.com>
2023-04-28 15:30:58 +0200

    Configure ConsoleApp to use database test

--- app/src/main/kotlin/com/agilogy/timetracking/driveradapters/console/ConsoleApp.kt
+++ app/src/main/kotlin/com/agilogy/timetracking/driveradapters/console/ConsoleApp.kt
@@ -2,22 +2,23 @@ package com.agilogy.timetracking.driveradapters.console
 
 import arrow.core.raise.effect
 import arrow.core.raise.fold
-import arrow.fx.coroutines.use
 import com.agilogy.db.hikari.HikariCp
 import com.agilogy.time.toInstantRange
 import com.agilogy.time.toLocalDateRange
 import com.agilogy.timetracking.domain.TimeTrackingApp
 import com.agilogy.timetracking.domain.TimeTrackingAppPrd
 import com.agilogy.timetracking.drivenadapters.PostgresTimeEntriesRepository
+import kotlinx.coroutines.runBlocking
 import java.time.ZoneOffset
 
-suspend fun main(args: Array<String>): Unit =
-        HikariCp.dataSource("jdbc:postgresql://localhost/", "postgres", "postgres").use { dataSource ->
+fun main(args: Array<String>): Unit = runBlocking {
+    HikariCp.dataSource("jdbc:postgresql://localhost/test", "postgres", "postgres").use { dataSource ->
         val timeEntriesRepository = PostgresTimeEntriesRepository(dataSource)
         val timeTrackingApp = TimeTrackingAppPrd(timeEntriesRepository)
         println("Your current zone id is ${ZoneOffset.systemDefault()}")
         ConsoleApp(ArgsParser(), timeTrackingApp, Console()).main(args)
     }
+}
 
 class ConsoleApp(
     private val argsParser: ArgsParser,
