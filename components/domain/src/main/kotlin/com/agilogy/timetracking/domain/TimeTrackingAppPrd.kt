d27fa67053c390fb416a75a4d336c81c860be570
Jordi Pradel <jordi.pradel@agilogy.com>
2023-05-05 11:42:10 +0200

    Document some to-dos to do as next steps

--- components/domain/src/main/kotlin/com/agilogy/timetracking/domain/TimeTrackingAppPrd.kt
+++ components/domain/src/main/kotlin/com/agilogy/timetracking/domain/TimeTrackingAppPrd.kt
@@ -5,12 +5,31 @@ import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalTime
 
+// TODO: Redesign TimeTrackingAppPrd dates:
+//  - Report parameters should use LocalDate, Month, etc. and a(n optional?) user timezone
+//  - Report responses should also use LocalDate et al. in the specified timezone
+//  - The LocalDate <-> Instant conversions should follow DRY as far as possible.
+//     - Specially avoid having them in both the DB and Kotlin
+// TODO: Authentication & authorization
 class TimeTrackingAppPrd(private val timeEntriesRepository: TimeEntriesRepository) : TimeTrackingApp {
 
+    // TODO: CRUD projects
+
+    // TODO: CRUD developers
+
+    // TODO: saveTimeEntries validation:
+    //  - Does project exist?
+    //  - Does developer exist?
+    //  - Are the time entries not overlapping?
+    //  - Are the time entries under the maximum duration (let's say 12h)?
+    // TODO: saveTimeEntries normalization
+    //  - Merge together consecutive time entries
     override suspend fun saveTimeEntries(developer: DeveloperName, timeEntries: List<Pair<ProjectName, ClosedRange<Instant>>>) {
         timeEntriesRepository.saveTimeEntries(timeEntries.map { TimeEntry(developer, it.first, it.second) })
     }
 
+    // TODO: Redesign getDeveloperHours to take a ClosedRange<LocalDate> and a timezone
+    // TODO: getDeveloperHours
     override suspend fun getDeveloperHours(range: ClosedRange<Instant>): Map<Pair<DeveloperName, ProjectName>, Hours> =
         timeEntriesRepository.getHoursByDeveloperAndProject(range)
 
